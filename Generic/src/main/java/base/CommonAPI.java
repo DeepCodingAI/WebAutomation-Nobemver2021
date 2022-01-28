4cd247af7fe7fb0bfa954ea14f2ef7012900d105
mrahman <rahmanww@gmail.com>
2022-01-27 20:39:40 -0500

    extent reporting

--- Generic/src/main/java/base/CommonAPI.java
+++ Generic/src/main/java/base/CommonAPI.java
@@ -1,5 +1,8 @@
 package base;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,22 +12,81 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import reporting.ExtentManager;
+import reporting.ExtentTestManager;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CommonAPI {
 
+    //Extent Report
+    public static ExtentReports extent;
+    @BeforeSuite
+    public void extentSetup(ITestContext context) {
+        ExtentManager.setOutputDirectory(context);
+        extent = ExtentManager.getInstance();
+    }
+    @BeforeMethod
+    public void startExtent(Method method) {
+        String className = method.getDeclaringClass().getSimpleName();
+        String methodName = method.getName().toLowerCase();
+        ExtentTestManager.startTest(method.getName());
+        ExtentTestManager.getTest().assignCategory(className);
+    }
+    protected String getStackTrace(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
+        return sw.toString();
+    }
+    @AfterMethod
+    public void afterEachTestMethod(ITestResult result) {
+        ExtentTestManager.getTest().getTest().setStartedTime(getTime(result.getStartMillis()));
+        ExtentTestManager.getTest().getTest().setEndedTime(getTime(result.getEndMillis()));
+
+        for (String group : result.getMethod().getGroups()) {
+            ExtentTestManager.getTest().assignCategory(group);
+        }
+
+        if (result.getStatus() == 1) {
+            ExtentTestManager.getTest().log(LogStatus.PASS, "Test Passed");
+        } else if (result.getStatus() == 2) {
+            ExtentTestManager.getTest().log(LogStatus.FAIL, getStackTrace(result.getThrowable()));
+        } else if (result.getStatus() == 3) {
+            ExtentTestManager.getTest().log(LogStatus.SKIP, "Test Skipped");
+        }
+        ExtentTestManager.endTest();
+        extent.flush();
+        if (result.getStatus() == ITestResult.FAILURE) {
+            captureScreenshot(driver, result.getName());
+        }
+        driver.quit();
+    }
+    @AfterSuite
+    public void generateReport() {
+        extent.close();
+    }
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+    //webDriver
     public String browserStackUserName = "your browserstack user name";
     public String browserStackAccessKey = "your browserstack access key";
     public String sauceLabsUserName = "your sace lab user name";
@@ -263,10 +325,10 @@ public class CommonAPI {
             ;
         }
     }
-//    public static String convertToString(String st) {
-//        String splitString = "";
-//        splitString = StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(st), ' ');
-//        return splitString;
-//    }
+    public static String convertToString(String st) {
+        String splitString = "";
+        splitString = StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(st), ' ');
+        return splitString;
+    }
 
 }
--- NYP/src/main/java/pages/AllFunctionality.java
+++ NYP/src/main/java/pages/AllFunctionality.java
@@ -5,6 +5,7 @@ import datafetch.FetchTheSteps;
 import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import reporting.TestLogger;
 
 import java.io.IOException;
 
@@ -18,6 +19,7 @@ public class AllFunctionality {
     String captureUrl = "https://nypost.com/";
 
     public void logIn(WebDriver driver){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         landingPage = PageFactory.initElements(driver, LandingPage.class);
         logInPage = PageFactory.initElements(driver, LogInPage.class);
         landingPage.clickOnLogIn();
@@ -26,6 +28,7 @@ public class AllFunctionality {
         //logInPage.clickOnLogInSubmitButton();
     }
     public void search(WebDriver driver){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         landingPage = PageFactory.initElements(driver, LandingPage.class);
         landingPage.clickOnSearch();
         searchPage = PageFactory.initElements(driver, SearchPage.class);
@@ -33,11 +36,13 @@ public class AllFunctionality {
     }
 
     public void clickOnSectionMenu(WebDriver driver)throws InterruptedException {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         landingPage = PageFactory.initElements(driver, LandingPage.class);
         landingPage.clickOnSectionMenu();
     }
 
     public void sectionsMenu(WebDriver driver, String capturedUrl)throws InterruptedException {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         try {
             clickOnSectionMenu(driver);
         }catch (Exception ex){
@@ -54,6 +59,7 @@ public class AllFunctionality {
     }
 
     public void runAllTheFeatureTest(WebDriver driver) throws InterruptedException, IOException {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         FetchTheSteps fetchTheSteps = new FetchTheSteps();
         String[] featureSteps = fetchTheSteps.getDataFromExcelFile();
         for (int i=1; i<featureSteps.length; i++){
@@ -61,7 +67,7 @@ public class AllFunctionality {
         }
     }
     public void select(String featureName, WebDriver driver)throws InterruptedException, IOException {
-
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         switch(featureName){
             case "signUp":
                 logIn(driver);
